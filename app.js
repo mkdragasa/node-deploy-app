8cd87b2d0037ba342afcd2cfab05a7b7a4e56c63
mkdragasa <michaela_ragasa@dlsu.edu.ph>
2020-04-20 22:08:58 +0800

    1

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ const authRouter = require('./routes/auth');
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/postRoutes');
 
+const { envPort, sessionKey } = require('./config');
 // Creates the express application
 const app = express();
-const port = 9090;
+const port = envPort || 9090;
+
 
 // Listening to the port provided
 app.listen(port, () => {
@@ -60,7 +62,7 @@ app.use(express.static('public'));
 // Insert server configuration after this comment
 // Sessions
 app.use(session({
-  secret: 'somegibberishsecret',
+  secret: sessionKey,
   store: new MongoStore({ mongooseConnection: mongoose.connection }),
   resave: false,
   saveUninitialized: true,
